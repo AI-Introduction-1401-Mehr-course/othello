d32f3a5b4138e45518d27cd2f56b6f1983428074
Mohammad Karami <1380mkkarami@gmail.com>
2022-12-31 12:49:03 +0330

    Switch to two-player zero-sum game terminology

--- othello/othello_game.py
+++ othello/othello_game.py
@@ -1,17 +1,134 @@
-from safe_typing import Callable, Dict
+from copy import deepcopy
+from enum import IntEnum
+from itertools import filterfalse, product
+from abstracts import Game
+from safe_typing import Self, List, Tuple, Dict
 
-from .othello_state_space import OthelloStateSpace, Side
+
+class Side(IntEnum):
+    DARK = 1
+    LIGHT = 2
+
+    @property
+    def other(self):
+        return Side(3 ^ self)
 
 
-class OthelloGame:
-    Player = Callable[[OthelloStateSpace], OthelloStateSpace.Action]
+class Cell(IntEnum):
+    EMPTY = 0
+    DARK_DISK = 1
+    LIGHT_DISK = 2
 
-    def __init__(self, players: Dict[Side, Player]):
-        self.board_state = OthelloStateSpace.initial_board()
-        self.players = players
 
-    def run(self):
-        while not self.board_state.is_goal():
-            action = self.players[self.board_state.playing_side](self.board_state)
-            self.board_state = self.board_state.result(action)
-        return self.board_state.value
+class OthelloGame(Game):
+    n = 8
+
+    directions = tuple(
+        filterfalse(lambda x: x == (0, 0), product(range(-1, 2), range(-1, 2)))
+    )
+
+    class Play(Game.Action):
+        x: int
+        y: int
+        disk_side: Side
+
+    class Pass(Game.Action):
+        pass
+
+    Action = Play | Pass
+
+    class State(Game.State):
+        board: List[List[Cell]]
+        player_turn: Side
+
+    @classmethod
+    def S0(cls) -> State:
+        mid = cls.n // 2
+        board = [[Cell.EMPTY for __ in range(cls.n)] for _ in range(cls.n)]
+        board[mid - 1][mid - 1] = Cell.LIGHT_DISK
+        board[mid - 1][mid] = Cell.DARK_DISK
+        board[mid][mid - 1] = Cell.DARK_DISK
+        board[mid][mid] = Cell.LIGHT_DISK
+
+        return cls.State(board, Side.DARK)
+
+    @classmethod
+    def inbound(cls, cell: Tuple[int, int]) -> bool:
+        return cell[0] < cls.n and cell[1] < cls.n and 0 <= cell[0] and 0 <= cell[1]
+
+    state: State
+
+    def __init__(self, state: State):
+        self.state = state
+
+    def result(self, action: Action) -> Self:
+        if isinstance(action, self.Pass):
+            return OthelloGame(
+                self.State(self.state.board, self.state.player_turn.other)
+            )
+
+        new_board = deepcopy(self.state.board)
+        cell_status = Cell(action.disk_side)
+
+        new_board[action.x][action.y] = cell_status
+        for direction in self.directions:
+            for i in range(1, self.n):
+                x = action.x + direction[0] * i
+                y = action.y + direction[1] * i
+                if not self.inbound((x, y)) or new_board[x][y] == Cell.EMPTY:
+                    break
+                if new_board[x][y] == cell_status:
+                    for j in range(i, 0, -1):
+                        x = action.x + direction[0] * j
+                        y = action.y + direction[1] * j
+                        new_board[x][y] = cell_status
+                    break
+        return OthelloGame(self.State(new_board, self.state.player_turn.other))
+
+    def action(self) -> List[Action]:
+        board = self.state.board
+        player_disk_cell_status = Cell(self.state.player_turn)
+
+        ans = set()
+        for cell in (
+            (x, y)
+            for x in range(self.n)
+            for y in range(self.n)
+            if board[x][y] == player_disk_cell_status
+        ):
+            for direction in self.directions:
+                x = cell[0] + direction[0]
+                y = cell[1] + direction[1]
+                if self.inbound((x, y)) and board[cell[0] + direction[0]][
+                    cell[1] + direction[1]
+                ] == Cell(self.to_move().other):
+                    for i in range(2, self.n):
+                        x = cell[0] + direction[0] * i
+                        y = cell[1] + direction[1] * i
+                        if (
+                            not self.inbound((x, y))
+                            or board[x][y] == player_disk_cell_status
+                        ):
+                            break
+                        if board[x][y] == Cell.EMPTY:
+                            ans.add(self.Play(x, y, self.state.player_turn))
+                            break
+
+        return list(ans) if ans else [self.Pass()]
+
+    def is_terminal(self) -> bool:
+        if self.action() == [self.Pass()]:
+            return self.result(self.Pass()).action() == [self.Pass()]
+        return False
+
+    def to_move(self) -> Game.Player:
+        return Game.Player(self.state.player_turn)
+
+    def utility(self) -> Dict[Game.Player, int]:
+        ans = {i: 0 for i in Game.Player}
+        for i in range(self.n):
+            for j in range(self.n):
+                if self.state.board[i][j] != Cell.EMPTY:
+                    ans[Game.Player(self.state.board[i][j])] += 1
+                    ans[Game.Player(self.state.board[i][j]).other] -= 1
+        return ans
