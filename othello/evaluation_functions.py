415d7ccdb62a5852bf33f06937f0b0ade1a60275
Nima Shadman <nima.shadman619@gmail.com>
2023-01-04 18:35:29 +0330

    Update Evaluation Functions

--- othello/evaluation_functions.py
+++ othello/evaluation_functions.py
@@ -1,33 +1,33 @@
-from .othello_state_space import *
+from .othello_game import *
 
 
-def evaluate_by_material(state_space: OthelloStateSpace):
+def evaluate_by_material(game: OthelloGame):
 
     light_disks_count = sum(
-        1 for cell in state_space.state.board if cell == Cell.LIGHT_DISK
+        1 for cell in game.state.board if cell == Cell.LIGHT_DISK
     )
     dark_disks_count = sum(
-        1 for cell in state_space.state.board if cell == Cell.DARK_DISK
+        1 for cell in game.state.board if cell == Cell.DARK_DISK
     )
     return dark_disks_count - light_disks_count
 
 
-def evaluate_by_mobility(state_space: OthelloStateSpace):
+def evaluate_by_mobility(game: OthelloGame):
     
-    turn = state_space.state.player_turn
+    turn = game.state.player_turn
     if turn == 1:
-        dark_possible_moves = state_space.action().count()
-        new_state_space = OthelloStateSpace(
-            state_space.State(
-                state_space.State.board, state_space.state.player_turn.other
+        dark_possible_moves = game.action().count()
+        new_state_space = OthelloGame(
+            game.State(
+                game.State.board, game.state.player_turn.other
             )
         )
         light_possible_moves = new_state_space.action().count()
     else:
-        light_possible_moves = state_space.action().count()
-        new_state_space = OthelloStateSpace(
-            state_space.State(
-                state_space.State.board, state_space.state.player_turn.other
+        light_possible_moves = game.action().count()
+        new_state_space = OthelloGame(
+            game.State(
+                game.State.board, game.state.player_turn.other
             )
         )
         dark_possible_moves = new_state_space.action().count()
@@ -35,6 +35,6 @@ def evaluate_by_mobility(state_space: OthelloStateSpace):
     return dark_possible_moves - light_possible_moves
 
 
-def evaluate_by_mobility_and_material(state_space: OthelloStateSpace):
+def evaluate_by_mobility_and_material(state_space: OthelloGame):
     
     return evaluate_by_mobility + evaluate_by_material
