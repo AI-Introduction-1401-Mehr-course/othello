e43282e55387f74e76b96988a6b12b4886e1181f
Mohammad Karami <1380mkkarami@gmail.com>
2023-01-06 00:37:58 +0330

    Test runner-player system

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from othello import OthelloGame
-from search import MinimaxSearch, AlphaBetaPruningSearch
-from datetime import datetime
+from othello_players.ai_player import AIPlayer
+from othello_players.cli_player import OthelloCLIPlayer
+from runner import Runner
+from search import AlphaBetaPruningSearch
 
 OthelloGame.n = 4  # Smaller board so it can actually return something.
 
-for S in (MinimaxSearch, AlphaBetaPruningSearch):
-    search = S()
-    start = datetime.now()
-    print("Timing " + S.__name__ + "...")
-    print(search(OthelloGame(OthelloGame.S0())))
-    print(datetime.now() - start)
+Runner(
+    OthelloGame(OthelloGame.S0()),
+    {
+        OthelloGame.Player.PLAYER2: AIPlayer(AlphaBetaPruningSearch()),
+        OthelloGame.Player.PLAYER1: OthelloCLIPlayer(),
+    },
+).run()
