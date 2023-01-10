5d44547234419a881a1b0391fda4ed36cf99c751
Nima Shadman <nima.shadman619@gmail.com>
2023-01-10 21:00:36 +0330

    Edit evaluate by mobility

--- othello/evaluation_functions.py
+++ othello/evaluation_functions.py
@@ -5,31 +5,11 @@ evaluate_by_material = OthelloGame.utility
 
 def evaluate_by_mobility(game: OthelloGame):
     ans = {}
-    ans[game.to_move()] = len(game.action())
     new_game = game.result(OthelloGame.Pass())
-    ans[new_game.to_move()] = len(new_game.action())
+    ans[game.to_move()] = len(game.action()) - len(new_game.action())
+    ans[new_game.to_move()] = len(new_game.action()) - len(game.action())
     return ans
-    # turn = game.state.player_turn
 
-    # if turn == 1:
-    #     dark_possible_moves = len(game.action())
-    #     new_game = OthelloGame(
-    #         game.State(
-    #             game.State.board, game.state.player_turn.other
-    #         )
-    #     )
-    #     # new_game = game.result(OthelloGame.Pass())
-    #     light_possible_moves = len(new_game.action())
-    # else:
-    #     light_possible_moves = len(game.action())
-    #     new_game = OthelloGame(
-    #         game.State(
-    #             game.State.board, game.state.player_turn.other
-    #         )
-    #     )
-    #     dark_possible_moves = list.count(new_game.action())
-
-    # return dark_possible_moves - light_possible_moves
 
 
 def evaluate_by_mobility_and_material(game: OthelloGame):
